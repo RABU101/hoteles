4ef368c16950db16cbf47fd5c68a42a4fc4c19ac
RABU <rabu.networking@gmail.com>
2021-10-26 15:03:21 -0300

    primera pagina conectada con variable

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,6 @@
+class RolesController < ApplicationController
+    def crear
+        #mostrar formulario para guardar roles nuevos
+        @mi_nombre = "Rodrigo Bravo Urzúa"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # dos primeras rutas obligatorias
+  # la tercera es opcional
+  #    ruta de ingreso url to: < metodo >
+  get 'roles/nuevo', to: 'roles#crear'
 end
