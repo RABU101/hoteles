f338ce39be338da0fb61438af44bc60fc33859cf
RABU <rabu.networking@gmail.com>
2021-10-28 05:24:58 -0300

    Agregando form para un rol

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,21 @@
+# Reciben peticiones y devuelven vistas
 class RolesController < ApplicationController
+    # GET /roles/nuevo
     def crear
         #mostrar formulario para guardar roles nuevos
         @mi_nombre = "Rodrigo Bravo Urzúa"
+        @rol = Rol.new
+    end
+    # POST /roles
+    def guardar
+        # Extraer los datos del params
+        datos_rol = params.require(:rol).permit(:rol)
+        # Asignando los datos en un nuevo rol
+        @nuevo_rol = Rol.new(datos_rol)
+        if @nuevo_rol.save
+            # por verdad mostrar algo redirect_to
+        else
+            # sino, tomar prestada una vista render
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # dos primeras rutas obligatorias
   # la tercera es opcional
   #    ruta de ingreso url to: < metodo >
-  get 'roles/nuevo', to: 'roles#crear'
+  get   'roles/nuevo',  to: 'roles#crear'
+  post  'roles',        to: 'roles#guardar'
 end
